78a5996b787317699893d78a596194b8e9b0080e
Дмитрий Карих <the.dr.hax@gmail.com>
2017-10-01 17:30:07 +0300

    entrypoint: Escape all variables (fixes #1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$AVD" ]; then
-    socat tcp-listen:5555,bind=127.0.0.1,fork tcp:$AVD & SOCAT_PID=$!
+    socat tcp-listen:5555,bind=127.0.0.1,fork tcp:"$AVD" & SOCAT_PID=$!
 
     echo "Waiting for AVD to become online..."
     while ! adb devices | sed -n 2p | grep -q device; do
@@ -12,13 +12,13 @@ fi
 
 java -jar /swarm.jar \
     -disableClientsUniqueId \
-    -username $JENKINS_MASTER_USERNAME \
-    -password $JENKINS_MASTER_PASSWORD \
-    -mode $JENKINS_SLAVE_MODE \
-    -name $JENKINS_SLAVE_NAME \
-    -executors $JENKINS_SLAVE_WORKERS \
-    -master $JENKINS_MASTER_URL \
-    -fsroot $JENKINS_SLAVE_ROOT \
+    -username "$JENKINS_MASTER_USERNAME" \
+    -password "$JENKINS_MASTER_PASSWORD" \
+    -mode "$JENKINS_SLAVE_MODE" \
+    -name "$JENKINS_SLAVE_NAME" \
+    -executors "$JENKINS_SLAVE_WORKERS" \
+    -master "$JENKINS_MASTER_URL" \
+    -fsroot "$JENKINS_SLAVE_ROOT" \
     -labels "$JENKINS_SLAVE_LABELS swarm"
 
 if [ "$AVD_PID" ]; then
