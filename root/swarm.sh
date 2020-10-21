11ad8ec9c7ab8af440682dea86a5328dc21e138d
Дмитрий Карих <the.dr.hax@gmail.com>
2020-10-22 00:43:47 +0300

    Download agent into SLAVE_ROOT

--- root/swarm.sh
+++ root/swarm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget "$MASTER_URL/jnlpJars/agent.jar" -O /agent.jar || exit $?
+wget "$MASTER_URL/jnlpJars/agent.jar" -O "$SLAVE_ROOT/agent.jar" || exit $?
 
-java -jar /agent.jar \
+java -jar "$SLAVE_ROOT/agent.jar" \
     -jnlpUrl "$MASTER_URL/computer/$SLAVE_NAME/slave-agent.jnlp" \
     -secret "$SLAVE_SECRET" \
     -workDir "$SLAVE_ROOT"
